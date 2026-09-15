db32e8db1b7e20830819a350546012f02ac9f587
Fa1tloy <baranovdaniil534@gmail.com>
2026-09-15 09:04:50 +0300

    MDK 01.01

---
+++ Лабороторная работа/Лабороторная работа/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабороторная_работа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double orderCost = ReadPositiveDouble("Введите стоимость заказа (руб): ");
+            double distance = ReadPositiveDouble("Введите расстояние доставки (км): ");
+            int hour = ReadHour("Введите время заказа (час, 0-23): ");
+
+            double deliveryCost = CalculateDeliveryCost(orderCost, distance, hour);
+            double totalAmount = orderCost + deliveryCost;
+
+            Console.WriteLine($"Стоимость доставки: {deliveryCost} руб");
+            Console.WriteLine($"Итого к оплате: {totalAmount} руб");
+        }
+
+        static double ReadPositiveDouble(string message)
+        {
+            double value;
+            while (true)
+            {
+                Console.Write(message);
+                string input = Console.ReadLine();
+                if (double.TryParse(input, out value) && value >= 0)
+                {
+                    return value;
+                }
+                Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+            }
+        }
+
+        static int ReadHour(string message)
+        {
+            int hour;
+            while (true)
+            {
+                Console.Write(message);
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out hour) && hour >= 0 && hour <= 23)
+                {
+                    return hour;
+                }
+                Console.WriteLine("Некорректный ввод. Введите число от 0 до 23.");
+            }
+        }
+
+        static double CalculateDeliveryCost(double orderCost, double distance, int hour)
+        {
+            const double basePrice = 150;
+            const double additionalKmPrice = 50;
+            const double peakMultiplier = 1.3;
+
+            double totalPrice = 0;
+
+            if (orderCost >= 2000)
+            {
+                totalPrice = 0; 
+            }
+            else
+            {
+                totalPrice = basePrice;
+                if (distance > 3)
+                {
+                    totalPrice += (distance - 3) * additionalKmPrice;
+                }
+            }
+
+            if (IsPeakHour(hour))
+            {
+                totalPrice *= peakMultiplier;
+            }
+
+            return Math.Round(totalPrice);
+        }
+
+        static bool IsPeakHour(int hour)
+        {
+            return (hour >= 12 && hour < 14) || (hour >= 18 && hour < 20);
+        }
+    }
+}
---
+++ Лабороторная работа/Лабороторная работа/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Лабороторная работа")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Лабороторная работа")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e3ea849b-dfc6-461e-94f8-40d0b732cadb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
